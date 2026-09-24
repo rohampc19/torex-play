3bcf6994ae1354eca905c80e3714086118fcb4da
rohampc19 <roham9819@gmail.com>
2026-09-24 19:30:20 +0330

    chore: complete migration foundation

---
+++ server/src/server.js
@@ -0,0 +1,1 @@
+import http from 'node:http'; import {createApp} from './app.js'; import {env} from './config/env.js'; const app=createApp(); const server=http.createServer(app); server.listen(env.PORT,()=>console.log('TOREX PLAY API listening on '+env.PORT));
